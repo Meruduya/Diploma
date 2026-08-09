58e825315ae621c8ab1e8e46241523a7f96e2d06
Veronika <korynaitroyd@gmail.com>
2026-08-09 15:03:38 +0300

    Set up Allure reporting

---
+++ app/src/androidTest/java/ru/iteco/fmhandroid/ui/AllureAndroidJUnitRunner.kt
@@ -0,0 +1,7 @@
+package ru.iteco.fmhandroid.ui
+
+
+import io.qameta.allure.android.runners.AllureAndroidJUnitRunner as BaseRunner
+
+
+class AllureAndroidJUnitRunner : BaseRunner()
--- app/src/androidTest/java/ru/iteco/fmhandroid/ui/AuthTest.kt
+++ app/src/androidTest/java/ru/iteco/fmhandroid/ui/AuthTest.kt
@@ -2,22 +2,28 @@ package ru.iteco.fmhandroid.ui
 
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.action.ViewActions.click
-import androidx.test.espresso.assertion.ViewAssertions.matches
-import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
 import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.espresso.matcher.ViewMatchers.withText
 import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import io.qameta.allure.kotlin.Description
+import io.qameta.allure.kotlin.Epic
+import io.qameta.allure.kotlin.Feature
+import io.qameta.allure.kotlin.Story
+import io.qameta.allure.kotlin.junit4.DisplayName
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 import ru.iteco.fmhandroid.R
 import ru.iteco.fmhandroid.ui.data.TestData
-import ru.iteco.fmhandroid.ui.pages.AuthPage
 import ru.iteco.fmhandroid.ui.data.Waiters
+import ru.iteco.fmhandroid.ui.pages.AuthPage
+import org.hamcrest.Matchers.anyOf
 
 @RunWith(AndroidJUnit4::class)
+@Epic("UI-тестирование приложения «Мобильный хоспис»")
+@Feature("Авторизация")
 class AuthTest {
 
     @get:Rule
@@ -27,30 +33,41 @@ class AuthTest {
 
     @Before
     fun prepareAuthScreen() {
+        Waiters.waitForDisplayedView(
+            anyOf(withId(R.id.enter_button), withId(R.id.main_menu_image_button)),
+            20000
+        )
 
-        Waiters.waitForView(withId(R.id.main_menu_image_button), 15000)
         try {
             onView(withId(R.id.authorization_image_button)).perform(click())
             onView(withText("Log out")).perform(click())
         } catch (e: Exception) {
-
         }
 
         authPage.waitForAuthScreen()
     }
 
     @Test
+    @Story("Отображение экрана авторизации")
+    @DisplayName("Экран авторизации отображается после выхода из аккаунта")
+    @Description("Проверяет, что после выхода из аккаунта открывается экран авторизации с доступной кнопкой входа")
     fun authScreenIsDisplayedAfterLogout() {
         authPage.checkAuthScreenIsDisplayed()
     }
 
     @Test
+    @Story("Вход в приложение")
+    @DisplayName("Успешный вход с корректными учётными данными")
+    @Description("Проверяет, что при вводе корректных логина и пароля выполняется вход и открывается главный экран приложения")
     fun loginWithValidCredentialsShouldSucceed() {
         authPage.login(TestData.VALID_LOGIN, TestData.VALID_PASSWORD)
-        Waiters.waitForView(withId(R.id.main_menu_image_button), 20000)
+        Waiters.waitForDisplayedView(withId(R.id.main_menu_image_button), 20000)
     }
 
     @Test
+    @Story("Вход в приложение")
+    @DisplayName("Отказ во входе при неверном пароле")
+    @Description("Проверяет, что при вводе неверного пароля вход не выполняется и пользователь остаётся на экране авторизации")
     fun loginWithInvalidPasswordShouldFail() {
         authPage.login(TestData.VALID_LOGIN, TestData.INVALID_PASSWORD)
         authPage.checkAuthScreenIsDisplayed()
